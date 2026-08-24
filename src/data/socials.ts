7d391782e732b83a368c8760f46448b9b653c0ed
Amasha Das <miniamasha20@gmail.com>
2026-08-24 17:21:11 -0500

    Torch effect

---
+++ src/data/socials.ts
@@ -0,0 +1,23 @@
+export const socials = [
+  { label: "X", href: "https://x.com/hotelespir", icon: "x.svg" },
+  {
+    label: "Instagram",
+    href: "https://instagram.com/hotelespir",
+    icon: "instagram.svg",
+  },
+  {
+    label: "Bluesky",
+    href: "https://bsky.app/profile/hotelespir.bsky.social",
+    icon: "bluesky.svg",
+  },
+  {
+    label: "YouTube",
+    href: "https://youtube.com/@hotelespir",
+    icon: "youtube.svg",
+  },
+  {
+    label: "Discord",
+    href: "https://hotelespir.com/discord",
+    icon: "discord.svg",
+  },
+];

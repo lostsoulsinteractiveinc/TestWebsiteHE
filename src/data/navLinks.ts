ed1f4c0663b4db8097cfcee173b8cbd7741a1d9b
Amasha Das <miniamasha20@gmail.com>
2026-08-27 12:59:02 -0500

    Some more changes
    Sticky Nav

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -1,5 +1,5 @@
 export const navLinks = [
   { label: "News", path: "/news" },
-  { label: "Press Kit", path: "/press" },
   { label: "About", path: "/about" },
+  { label: "Press Kit", path: "/press" },
 ];
